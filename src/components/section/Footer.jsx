3fa58d68487025e93a71d68556220d735d3fdc0e
Yoonjiji <fc0098@gmail.com>
2024-06-18 11:12:16 +0900

    메인 콘텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer id='footer' role='contentinfo'>
-            Footer
+            <a href="mailto:webstoryboy@naver.com" rel='noopenner noreferrer'>
+                webstoryboy@naver.com
+            </a>
         </footer>
     )
 }
