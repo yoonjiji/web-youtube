cd0b8ddfaf62280fb103ac7f38526e1854e7d257
Yoonjiji <fc0098@gmail.com>
2024-06-19 01:23:08 +0900

    Swiper 이미지 슬라이드 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,8 @@ import { websiteText } from '../data/website'
 import { gsapText } from '../data/gsap'
 import { portfolioText } from '../data/portfolio'
 import { youtubeText } from '../data/youtube'
+import { developerText } from '../data/developer';
+import { todayText } from '../data/today'
 
 const Home = () => {
     return (
@@ -16,8 +18,8 @@ const Home = () => {
             title = "웹스토리보이 유튜브"
             description="웹스토리보이 유튜버 사이트에 오신 것을 환영합니다.">
             
-            <Today />
-            <Developer />
+            <Today videos={todayText} id="today"/>
+            <Developer videos={developerText} title="😪 추천 개발자를 소개합니다." id="developer" />
             <VideoSlider videos={webdText} title="😮 웹디자인기능사 준비는 이걸로!" id="webd" />
             <VideoSlider videos={websiteText} title="😛 웹표준 사이트 만들기 기초 다지기" id="website" />
             <VideoSlider videos={gsapText} title="🤓 GSAP 패럴랙스 효과를 하고 싶다면!" id="gsap" />
